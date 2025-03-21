20c6209fc8422dd379e3a0cd0e21dde730c12f6b
Евгений Власов <79114158852@mail.ru>
2025-03-21 07:00:36 +0300

    Clear

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,5 @@ use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $user = User::factory()->create();
-    $resource = Resource::factory()->create();
-    $booking = new Booking;
-    $booking->resource_id = $resource->id;
-    $booking->user_id = $user->id;
-    $booking->start_time = date('Y-m-d H:i:s');
-    $booking->end_time = date('Y-m-d H:i:s');
-    $booking->save();
+    
 });
